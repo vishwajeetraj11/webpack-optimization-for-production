e3cdb78255c886cee925ddce1adad0a5ca91ba3f
vishwajeet-graphy <vishwajeet.raj@graphy.com>
2023-08-20 18:06:32 +0530

    refactor

---
+++ src/data.js
@@ -0,0 +1,24 @@
+import initialTodos from './todos.json'
+
+export function getAllTodos() {
+    return initialTodos
+}
+
+export function addTodo(todo) {
+    initialTodos.push(todo)
+}
+
+export function removeTodo(id) {
+    initialTodos = initialTodos.filter(function (item) {
+        return item.id !== id
+    })
+}
+
+export function updateTodo(id, completed) {
+    const itemIndex = initialTodos.findIndex(function (value) {
+        return value.id === id
+    })
+    initialTodos[itemIndex].completed = completed
+}
+
+
