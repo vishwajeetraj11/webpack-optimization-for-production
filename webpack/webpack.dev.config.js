581b1787d18fc212235d83205418c2f8fcbc2512
vishwajeet-graphy <vishwajeet.raj@graphy.com>
2023-08-21 23:15:32 +0530

    adding webpack dev server

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -1,6 +1,20 @@
 const common = require('./webpack.common.config.js');
 const { merge } = require('webpack-merge');
+const path = require('path')
 
 module.exports = merge(common, {
-    mode: 'development'
+    mode: 'development',
+    devServer: {
+        port: 9000,
+        static: {
+            directory: path.resolve(__dirname, '..'),
+        },
+        devMiddleware: {
+            index: 'index.html',
+            writeToDisk: true
+        },
+        client: {
+            overlay: true,
+        }
+    }
 })
