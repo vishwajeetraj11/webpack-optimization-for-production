efa2c58ac4b7610aff407cfac2fbe1f7692c62a4
vishwajeet-graphy <vishwajeet.raj@graphy.com>
2023-08-24 10:32:29 +0530

    Using Experimental featues and adding support in babel

--- src/js/event-handlers.js
+++ src/js/event-handlers.js
@@ -1,12 +1,22 @@
 import { renderTodos, clearNewTodoInput, getTodoId } from "./ui"
 import { getAllTodos, addTodo, removeTodo, updateTodo } from "./data"
+import { capitalize } from 'lodash'
 
 export function onLoadEventHandler() {
     renderTodos(getAllTodos())
 }
 
+function trim(value) {
+    return value
+        .replace(/^\s+/, '')
+        .replace(/\s+$/, '')
+        .replace(/\s{2,}/g, ' ');
+}
+
 export function newTodoEventHandler(event) {
     let text = event.target.value
+    // text = anotherFun(capitalize(trim(text)))
+    text = text |> trim |> capitalize;
     addTodo({
         id: Date.now(),
         text: text,
